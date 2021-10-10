6f7940b53aab41eab4c50a0684c93663f855b6cb
bebeji-nappa <masadecade@gmail.com>
2021-10-10 16:12:34 +0900

    0.1.12

--- src/Content.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Content:React.VFC<ContentProps> = ({children, align = "left", size = "medium"}) => {
-  return (
-    <div className={`Content${align && ` ${align}`}${size && ` size-${size}`}`}>{children}</div>
-  )
-}
