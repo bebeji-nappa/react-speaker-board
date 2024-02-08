bf75a5a0dd51e6eac4c67bf43eeb042a4565d761
bebeji-nappa <masadecade@gmail.com>
2024-02-09 06:50:56 +0900

    [ci skip] PDF ダウンロード機能を実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
 import ReactSpeakerBoard from "./ReactSpeakerBoard";
 import Title from "./Title";
 import Text from "./Text";
@@ -11,6 +15,7 @@ import TriangleUp from "./TriangleUp";
 import TriangleRight from "./TriangleRight";
 import TriangleDown from "./TriangleDown";
 import TriangleLeft from "./TriangleLeft";
+import AllSlidePdf from "./AllSlidePdf";
 
 export const useReactSpeakerBoard = () => {
   return {
@@ -27,5 +32,29 @@ export const useReactSpeakerBoard = () => {
     TriangleDown,
     TriangleLeft,
     Text,
+    AllSlidePdf,
   }
 };
+
+export const useSlideOutputFiles = () => {
+  const download = async (slide: (() => JSX.Element)[], fileName?: string) => {
+    const slideLength = slide.length;
+    let pdf = new jsPDF({
+      orientation: 'l',
+      unit: 'px',
+      format: 'a4',
+    });
+    for (let i = 1; i <= slideLength; i++) {
+      console.log(`slide-${i}`);
+      const target = document.getElementById(`slide-${i}`);
+      if (target === null) return;
+
+      const canvas = await html2canvas(target)
+      const imgData = canvas.toDataURL('image/svg', 1.0);
+      pdf.addImage(imgData, 'SVG', 0, 60, canvas.width / 6, canvas.height / 6, `slide-${i}`);
+      if (i !== slideLength) pdf.addPage();
+    }
+    pdf.save(fileName ?? "reactspeakerboard.pdf");
+  };
+  return { download };
+}
