c40c61585b43cd7c765e23e567c4a3cbf4fb04a7
bebeji-nappa <masadecade@gmail.com>
2024-02-10 23:46:57 +0900

    fix Arrow Comporent

--- src/components/Figure/ArrowLeft.tsx
+++ src/components/Figure/ArrowLeft.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Square from "./Square";
 import TriangleLeft from "./TriangleLeft";
+import Content from "../Content";
 
 const ArrowLeft: React.FC<ArrowLeftProps> = ({ width = "100px", color = "black" }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+    <Content horizontal="center" vertical="center" layout="horizontal">
       <TriangleLeft height="50px" size="60px" color={color} />
       <Square width={width} height="50px" color={color} />
-    </div>
+    </Content>
   )
 }
 
--- src/components/Figure/ArrowUp.tsx
+++ src/components/Figure/ArrowUp.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Square from "./Square";
 import TriangleUp from "./TriangleUp";
+import Content from "../Content";
 
 const ArrowUp: React.FC<ArrowUpProps> = ({ height = "100px", color = "black" }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <Content horizontal="center" vertical="center">
       <TriangleUp height="50px" size="60px" color={color} />
       <Square width="50px" height={height} color={color} />
-    </div>
+    </Content>
   )
 }
 
